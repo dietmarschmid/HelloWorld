950672600e8cb054d684f59c1e761e720bc4b634
dietmarschmid <45719202+dietmarschmid@users.noreply.github.com>
2018-12-15 11:54:22 +0100

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,7 @@
 #include <iostream>                                     // Ein- und Ausgabebibliothek
  
 int main(){                                             // Hauptfunktion
-    std::cout << "Hallo, du schöne Welt!" << std::endl; // Ausgabe
+    std::cout << "Hallo, du schöne Welt!" << std::endl; // Ausgabe1
   
     return 0;                                           // Optionale Rückgabe an das Betriebssystem
 }
