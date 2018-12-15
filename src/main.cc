0a13619f05c8c99b965652b73726c26db2b99e44
dietmarschmid <45719202+dietmarschmid@users.noreply.github.com>
2018-12-15 12:20:17 +0100

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,11 @@
 #include <iostream>                                   // Ein- und Ausgabebibliothek
 
-int addiere(int summand1, int summand2) {
+int addierex(int summand1, int summand2) {
 	return (summand1 + summand2);
 }
 
 int main(){                                             // Hauptfunktion
     std::cout << "Hallo, du schöne Welt!" << std::endl; // Ausgabe1
-    int summe = addiere(3, 7);
+    int summe = addierex(3, 7);
     return 0;                                           // Optionale Rückgabe an das Betriebssystem
 }
