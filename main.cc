2cdf567690ff49f0964852c982f046ebb1ac9fe4
dietmarschmid <45719202+dietmarschmid@users.noreply.github.com>
2018-12-15 11:07:37 +0100

    Delete main.cc

--- main.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>                                     // Ein- und Ausgabebibliothek
-2 
-3 int main(){                                             // Hauptfunktion
-4     std::cout << "Hallo, du schöne Welt!" << std::endl; // Ausgabe
-5 
-6     return 0;                                           // Optionale Rückgabe an das Betriebssystem
-7 }
