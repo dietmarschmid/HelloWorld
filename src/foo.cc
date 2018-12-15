b15a8129d86a9075b96a26feff3dfe3760778295
dietmarschmid <45719202+dietmarschmid@users.noreply.github.com>
2018-12-15 14:47:07 +0100

    Delete foo.cc

--- src/foo.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>                                     // Ein- und Ausgabebibliothek
- 
-int foo(){                                             // Hauptfunktion
-    std::cout << "Hallo, foo" << std::endl; // Ausgabe2
-  
-    return 0;                                           // Optionale Rückgabe an das Betriebssystem
-}
