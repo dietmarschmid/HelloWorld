cb934c27057178daf5af779bccfd8e8e3e4a29ad
dietmarschmid <45719202+dietmarschmid@users.noreply.github.com>
2018-12-15 12:51:42 +0100

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -6,9 +6,11 @@ int addierex(int summand1, int summand2) {
 }
 
 int main(){                                             	// Hauptfunktion
-    	std::cout << "Hallo, du schöne Welt!" << std::endl; 	// Ausgabe1
-   	int summex = addierex(3, 7);
-    	int summe = addiere(3, 7);
+	std::cout << "Hallo, du schöne Welt!" << std::endl; 	// Ausgabe1
+   	
+	int summe = addiere(3, 7);
+	int ergeb = subtrahiere(7, 3);
+	
 	printf( "test:  %d\n", summe );
-    	return 0;                                           	// Optionale Rückgabe an das Betriebssystem
+	return 0;                                           	// Optionale Rückgabe an das Betriebssystem
 }
