e4171308c883b279df0c7fe67e68f22ead752e79
Dietmar Schmid <dietmar.schmid@gmx.net>
2018-12-19 21:45:49 +0100

    ds

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,5 @@
 #include <iostream>         // Ein- und Ausgabebibliothek
-//   #include <stdio.h>          // Standardbibliothek
+#include <stdio.h>          // Standardbibliothek
 #include "funktionen.h"
 
 
@@ -29,9 +29,11 @@ int main(int argc, char *argv[])        /* Argumente an main-Funktion */
 {                                             	// Hauptfunktion
 	std::cout << "Hallo, du schöne Welt!" << std::endl; 	// Ausgabe1
     
-    field1 = argv[1];
+    int field1 = getopt(argv[1]);
     operand = argv[2];
     number2 = argv[3];
+    
+
     
 	int summe = addiere(number1, numnber2);
 	
